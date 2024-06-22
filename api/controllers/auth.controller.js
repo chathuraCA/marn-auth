cdf580274f5739d91ddfc7f2918a76e508a1e8fd
chathura <k11332.chathura@gmail.com>
2024-06-23 00:26:43 +0530

    create sing up api routes

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+export const singup =async(req,res)=>{
+    const{username,email,password}=req.body;
+    const hashpassword=bcryptjs.hashSync(password,10);
+    
+    const newUser =new User({username,email,password:hashpassword});
+  try{
+    await newUser.save();
+    res.status(201).json({message:"User created sucsessfull"});
+  }catch(error){
+res.status(500).json(error.message);
+  }
+  
+   
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 mongoose.connect(process.env.Mongo).then(()=>{
     console.log('Connected to mongo');
@@ -10,10 +12,12 @@ mongoose.connect(process.env.Mongo).then(()=>{
 });
 
 const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('Server listing on port 3000 !');
 });
 
 app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes);
 
 
