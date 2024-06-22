07476dd8e2af94ecfd1c18246d77e10ab5a799ae
chathura <k11332.chathura@gmail.com>
2024-06-22 22:05:53 +0530

    ceare api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.Mongo).then(()=>{
     console.log('Connected to mongo');
@@ -11,4 +12,8 @@ mongoose.connect(process.env.Mongo).then(()=>{
 const app=express();
 app.listen(3000,()=>{
     console.log('Server listing on port 3000 !');
-})
+});
+
+app.use("/api/user",userRoutes);
+
+
