c0069c0f1fb0efcd442f675cfa51bf229cf16431
chathura <k11332.chathura@gmail.com>
2024-06-22 16:18:10 +0530

    adding mongo db to project

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.Mongo).then(()=>{
+    console.log('Connected to mongo');
+}).catch((err)=>{
+    console.log(err);
+});
+
 const app=express();
 app.listen(3000,()=>{
     console.log('Server listing on port 3000 !');
